e3d8c184cfdedde0ae474a0b08aeaa8c2107ffb8
Oswaldo Gonzalez <83186962+oswaldo32s@users.noreply.github.com>
2025-06-11 21:31:50 -0600

    Update sub_category.sql

--- tables/sub_category.sql
+++ tables/sub_category.sql
@@ -4,7 +4,7 @@ CREATE TABLE finance.sub_category
   name              VARCHAR2(20)        NOT NULL,
   description       VARCHAR2(100),      --NULL
   category_id       NUMBER(6),          --NULL          
-  create_datetime   TIMESTAMP           NOT NULL,      
+  create_datetime   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,      
   update_datetime   TIMESTAMP,          --NULL
   CONSTRAINT fk_category FOREIGN KEY (category_id)
     REFERENCES category(id)
