846227a323f14b79d5f3e437b5487dabc383e258
Oswaldo Gonzalez <83186962+oswaldo32s@users.noreply.github.com>
2025-06-11 21:26:21 -0600

    Update sub_category.sql

--- tables/sub_category.sql
+++ tables/sub_category.sql
@@ -1,11 +1,11 @@
 CREATE TABLE finance.sub_category
 (
   id                NUMBER(6) GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name              VARCHAR2(20),
-  description       VARCHAR2(100),
-  category_id       NUMBER(6),
-  create_datetime   TIMESTAMP,
-  update_datetime   TIMESTAMP,
+  name              VARCHAR2(20)        NOT NULL,
+  description       VARCHAR2(100),      --NULL
+  category_id       NUMBER(6),          --NULL          
+  create_datetime   TIMESTAMP           NOT NULL,      
+  update_datetime   TIMESTAMP,          --NULL
   CONSTRAINT fk_category FOREIGN KEY (category_id)
     REFERENCES category(id)
     ON DELETE SET NULL
