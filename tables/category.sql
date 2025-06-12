8ba7cc1e13f8c1df7685f3f5f7d553a96767f233
Oswaldo Gonzalez <83186962+oswaldo32s@users.noreply.github.com>
2025-06-11 21:45:07 -0600

    Update and rename category.sql to categories.sql

--- tables/category.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE finance.category
-(
-  id                    NUMBER(6) GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name                  VARCHAR2(50)       NOT NULL,
-  description           VARCHAR2(100),     --NULL
-  create_datetime       TIMESTAMP          NOT NULL,
-  update_datetime       TIMESTAMP          --NULL
-);
