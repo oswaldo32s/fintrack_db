85cbd629534068289592a72c833eb4b9badf43ae
Oswaldo Gonzalez <83186962+oswaldo32s@users.noreply.github.com>
2025-06-11 21:24:23 -0600

    Update category.sql

--- tables/category.sql
+++ tables/category.sql
@@ -1,8 +1,8 @@
 CREATE TABLE finance.category
 (
-  id               NUMBER(6) GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name             VARCHAR2(20) CONSTRAINT name_nn NOT NULL,
-  description      VARCHAR2(100),
-  createDatetime   TIMESTAMP CONSTRAINT create_dt_nn NOT NULL,
-  updateDatetime   TIMESTAMP
+  id                    NUMBER(6) GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name                  VARCHAR2(50)       NOT NULL,
+  description           VARCHAR2(100),     --NULL
+  create_datetime       TIMESTAMP          NOT NULL,
+  update_datetime       TIMESTAMP          --NULL
 );
